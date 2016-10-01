d1928f6b570a349a2e2cf641c512eab14093484a
mattmonja <mattmonjarrez@gmail.com>
2016-10-01 16:21:14 +0000

    URL Reader implemented

--- app/src/main/java/setmatch/setmatch/NetworkManager.java
+++ app/src/main/java/setmatch/setmatch/NetworkManager.java
@@ -1,4 +1,31 @@
 package setmatch.setmatch;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import java.lang.String;
+import java.lang.StringBuffer;
+
 public class NetworkManager {
+
+
+    private static String readUrl(String urlString) throws Exception{
+        BufferedReader reader = null;
+        try{
+
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
 }
