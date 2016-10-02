0d9b7eaf196f69b6fcd7294f6649b21c3e7b34bf
mattmonja <mattmonjarrez@gmail.com>
2016-10-02 01:52:52 -0400

    This is the future we chose

---
+++ app/src/main/java/setmatch/setmatch/ObjectItem.java
@@ -0,0 +1,14 @@
+package setmatch.setmatch;
+
+public class ObjectItem {
+
+    public int itemId;
+    public String itemName;
+
+    // constructor
+    public ObjectItem(int itemId, String itemName) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+    }
+
+}
---
+++ app/src/main/java/setmatch/setmatch/OnItemClickListenerListViewItem.java
@@ -0,0 +1,38 @@
+package setmatch.setmatch;
+
+/**
+ * Created by Matthew on 10/2/2016.
+ */
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/*
+ * Here you can control what to do next when the user selects an item
+ */
+public class OnItemClickListenerListViewItem implements OnItemClickListener {
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Context context = view.getContext();
+
+        TextView textViewItem = ((TextView) view.findViewById(R.id.textViewItem));
+
+        // get the clicked item name
+        String listItemText = textViewItem.getText().toString();
+
+        // get the clicked item ID
+        String listItemId = textViewItem.getTag().toString();
+
+        // just toast it
+        Toast.makeText(context, "Item: " + listItemText + ", Item ID: " + listItemId, Toast.LENGTH_SHORT).show();
+
+        ((leaderboard) context).alertDialogStores.cancel();
+
+    }
+
+}
